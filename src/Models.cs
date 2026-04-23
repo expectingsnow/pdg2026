af36d4f590709eb47623e9cbc1621ab9b50f3c26
Florian Eckert <florian.eckert@gmail.com>
2026-04-23 08:44:26 +0200

    refactoring (#4)

--- src/Models.cs
+++ src/Models.cs
@@ -1,62 +1,58 @@
-public record RootObject(
-    string id,
-    int timestamp,
-    Dictionary<string, KeyFrame> keyframe
-);
+public record RootObject(Dictionary<string, KeyFrame> keyframe);
+
+public record Category(string name, string parent_key);
+
+public record StartBlock(string key);
+
+public record Athlete(string name, int birth_year, string nationality);
+
+public record CheckPoint(string key, string time);
 
 public record KeyFrame(
-    int id,
-    string timing_id,
     string bib,
     string name,
-    bool is_private,
     Category category,
-    Start_block start_block,
-    Athletes[] athletes,
-    string status,
-    string start,
+    StartBlock start_block,
+    Athlete[] athletes,
+    string status, 
     string time,
     int rank,
     int crank,
-    int trend,
-    double lat,
-    double lon,
-    object loc_time,
     CheckPoint[] checkpoints
-);
+)
+{
+    public TimeSpan? TimeAtCheckPoint(string checkPointKey)
+    {
+        var checkPoint = checkpoints.FirstOrDefault(c => c.key == checkPointKey);
+        
+        return TryParseIsoDuration(checkPoint?.time, out var timeSpan) ? timeSpan : null;
+    }
+    
+    public TimeSpan? TimeFromCheckPointToCheckPoint(string checkPointKeyFrom, string checkPointKeyTo)
+    {
+        var timeFrom = TimeAtCheckPoint(checkPointKeyFrom); if (timeFrom is null) { return null; }
+        var timeTo   = TimeAtCheckPoint(checkPointKeyTo  ); if (timeTo   is null) { return null; }
 
-public record Category(
-    int id,
-    int parent,
-    string key,
-    string name,
-    string parent_key
-);
+        return timeTo - timeFrom;
+    }
+    
+    public static bool TryParseIsoDuration(string? value, out TimeSpan? timeSpan)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            timeSpan = null;
+            return false;
+        }
 
-public record Start_block(
-    int id,
-    int race,
-    string key,
-    string start_date
-);
-
-public record Athletes(
-    int id,
-    int team,
-    string timing_id,
-    string name,
-    int birth_year,
-    string nationality,
-    int order,
-    bool is_lead,
-    bool is_standby
-);
-
-public record CheckPoint(
-    int id,
-    string key,
-    string time,
-    string status,
-    int rank,
-    int crank
-);
+        try
+        {
+            timeSpan = System.Xml.XmlConvert.ToTimeSpan(value);
+            return true;
+        }
+        catch (FormatException)
+        {
+            timeSpan = null;
+            return false;
+        }
+    }
+};
