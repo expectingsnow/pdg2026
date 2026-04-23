9768a4479fd54713350af0e2af7d3162d926fe7a
fleckert <flo.eckert@live.com>
2026-04-23 08:32:23 +0200

    .

--- src/Models.cs
+++ src/Models.cs
@@ -1,11 +1,19 @@
-public record RootObject( string id, int timestamp, Dictionary<string, KeyFrame> keyframe );
+public record RootObject(Dictionary<string, KeyFrame> keyframe);
+
+public record Category(string name, string parent_key);
+
+public record StartBlock(string key);
+
+public record Athlete(string name, int birth_year, string nationality);
+
+public record CheckPoint(string key, string time);
 
 public record KeyFrame(
     string bib,
     string name,
     Category category,
-    Start_block start_block,
-    Athletes[] athletes,
+    StartBlock start_block,
+    Athlete[] athletes,
     string status, 
     string time,
     int rank,
@@ -19,7 +27,6 @@ public record KeyFrame(
         
         return TryParseIsoDuration(checkPoint?.time, out var timeSpan) ? timeSpan : null;
     }
-
     
     public TimeSpan? TimeFromCheckPointToCheckPoint(string checkPointKeyFrom, string checkPointKeyTo)
     {
@@ -49,63 +56,3 @@ public record KeyFrame(
         }
     }
 };
-
-public record Category( string name, string parent_key);
-
-public record Start_block(string key);
-
-public record Athletes( string name, int birth_year, string nationality );
-
-public record CheckPoint(
-    string key,
-    string time
-);
-
-public record Datum(
-    string bib,
-    string name,
-    
-    string categoryParentKey,
-    string categoryName,
-    
-    string startBlockKey,
-    string status, 
-    string? time,
-    int? rank,
-    int crank,
-    
-    string? athlete1Name,
-    int? athlete1BirthYear,
-    string? athlete1Nationality,
-    string? athlete2Name,
-    int? athlete2BirthYear,
-    string? athlete2Nationality,
-    string? athlete3Name,
-    int? athlete3BirthYear,
-    string? athlete3Nationality,
-    
-    string checkPoint00,
-    string checkPoint01,
-    string time_01_02,
-    string checkPoint02,
-    string time_02_03,
-    string checkPoint03,
-    string time_03_04,
-    string checkPoint04,
-    string time_04_05,
-    string checkPoint05,
-    string time_05_06,
-    string checkPoint06,
-    string time_06_07,
-    string checkPoint07,
-    string time_07_08,
-    string checkPoint08,
-    string time_08_09,
-    string checkPoint09,
-    string time_09_10,
-    string checkPoint10,
-    string time_10_11,
-    string checkPoint11,
-    string time_11_12,
-    string checkPoint12
-);
--- src/Program.cs
+++ src/Program.cs
@@ -1,15 +1,13 @@
-﻿using System.Globalization;
-using System.Text;
-using System.Text.Json;
-using CsvHelper;
+﻿using System.Text.Json;
 
 Console.WriteLine("start");
 
+#region setup
 var directoryData = Path.Combine(Directory.GetCurrentDirectory(), "data");
+var pathOut = Path.Combine(directoryData, "pdg2026.csv");
 
 Directory.CreateDirectory(directoryData);
-
-var pathOut = Path.Combine(directoryData, "pdg2026.csv");
+#endregion
 
 #region download
 Console.WriteLine("--------------------");
@@ -57,8 +55,6 @@ foreach (var link in links)
 }
 #endregion
 
-
-
 #region load and transform
 Console.WriteLine("--------------------");
 Console.WriteLine("transform data");
@@ -67,74 +63,78 @@ var files = Directory
                       .OrderBy(p => p)
                       .ToArray();
 
-List<Datum> records = [];
+List<object> records = [];
 
 foreach (var file in files)
 {
-    var content = File.ReadAllText(file, Encoding.UTF8);
+    var content = File.ReadAllText(file, System.Text.Encoding.UTF8);
     var rootObject = JsonSerializer.Deserialize<RootObject>(content) ?? throw new Exception("Deserialize failed");
 
     records.AddRange(
-        rootObject.keyframe.Select(p => p.Value).Select(frame => new Datum(
-                bib                : frame.bib,
-                name               : frame.name,
-                categoryName        : frame.category.name,
-                categoryParentKey  : frame.category.parent_key,
-                startBlockKey      : frame.start_block.key,
-                
-                status             : frame.status, 
-                time               : KeyFrame.TryParseIsoDuration(frame.time, out var time) ? time.ToString() : null,
-                rank               : frame.rank == 0 ? null : frame.rank,
-                crank              : frame.crank,
-                
-                athlete1Name       : frame.athletes.Length > 0 ? frame.athletes[0].name        : null,
-                athlete1BirthYear  : frame.athletes.Length > 0 ? frame.athletes[0].birth_year  : null,
-                athlete1Nationality: frame.athletes.Length > 0 ? frame.athletes[0].nationality : null,
-                athlete2Name       : frame.athletes.Length > 1 ? frame.athletes[1].name        : null,
-                athlete2BirthYear  : frame.athletes.Length > 1 ? frame.athletes[1].birth_year  : null,
-                athlete2Nationality: frame.athletes.Length > 1 ? frame.athletes[1].nationality : null,
-                athlete3Name       : frame.athletes.Length > 2 ? frame.athletes[2].name        : null,
-                athlete3BirthYear  : frame.athletes.Length > 2 ? frame.athletes[2].birth_year  : null,
-                athlete3Nationality: frame.athletes.Length > 2 ? frame.athletes[2].nationality : null,
-              
-                checkPoint00       : frame.TimeAtCheckPoint("S0" )?.ToString() ?? string.Empty,
-                checkPoint01       : frame.TimeAtCheckPoint("S1" )?.ToString() ?? string.Empty,
-                checkPoint02       : frame.TimeAtCheckPoint("S2" )?.ToString() ?? string.Empty,
-                checkPoint03       : frame.TimeAtCheckPoint("S3" )?.ToString() ?? string.Empty,
-                checkPoint04       : frame.TimeAtCheckPoint("S4" )?.ToString() ?? string.Empty,
-                checkPoint05       : frame.TimeAtCheckPoint("S5" )?.ToString() ?? string.Empty,
-                checkPoint06       : frame.TimeAtCheckPoint("S6" )?.ToString() ?? string.Empty,
-                checkPoint07       : frame.TimeAtCheckPoint("S7" )?.ToString() ?? string.Empty,
-                checkPoint08       : frame.TimeAtCheckPoint("S8" )?.ToString() ?? string.Empty,
-                checkPoint09       : frame.TimeAtCheckPoint("S9" )?.ToString() ?? string.Empty,
-                checkPoint10       : frame.TimeAtCheckPoint("S10")?.ToString() ?? string.Empty,
-                checkPoint11       : frame.TimeAtCheckPoint("S11")?.ToString() ?? string.Empty,
-                checkPoint12       : frame.TimeAtCheckPoint("S12")?.ToString() ?? string.Empty,
+        rootObject.keyframe
+                    .Select(p => p.Value)
+                    .Select(frame => new {
+                        bib                = frame.bib,
+                        name               = frame.name,
+                        categoryName       = frame.category.name,
+                        categoryParentKey  = frame.category.parent_key,
+                        startBlockKey      = frame.start_block.key,
+                        
+                        status             = frame.status, 
+                        time               = KeyFrame.TryParseIsoDuration(frame.time, out var time) ? time.ToString() : null,
+                        rank               = (int?)(frame.rank == 0 ? null : frame.rank),
+                        crank              = frame.crank,
+                        
+                        athlete1Name       =        frame.athletes.Length > 0 ? frame.athletes[0].name        : null ,
+                        athlete1BirthYear  = (int?)(frame.athletes.Length > 0 ? frame.athletes[0].birth_year  : null),
+                        athlete1Nationality=        frame.athletes.Length > 0 ? frame.athletes[0].nationality : null ,
+                        athlete2Name       =        frame.athletes.Length > 1 ? frame.athletes[1].name        : null ,
+                        athlete2BirthYear  = (int?)(frame.athletes.Length > 1 ? frame.athletes[1].birth_year  : null),
+                        athlete2Nationality=        frame.athletes.Length > 1 ? frame.athletes[1].nationality : null ,
+                        athlete3Name       =        frame.athletes.Length > 2 ? frame.athletes[2].name        : null ,
+                        athlete3BirthYear  = (int?)(frame.athletes.Length > 2 ? frame.athletes[2].birth_year  : null),
+                        athlete3Nationality=        frame.athletes.Length > 2 ? frame.athletes[2].nationality : null ,
+                      
+                        checkPoint00       = frame.TimeAtCheckPoint("S0" )?.ToString() ?? string.Empty,
+                        checkPoint01       = frame.TimeAtCheckPoint("S1" )?.ToString() ?? string.Empty,
+                        checkPoint02       = frame.TimeAtCheckPoint("S2" )?.ToString() ?? string.Empty,
+                        checkPoint03       = frame.TimeAtCheckPoint("S3" )?.ToString() ?? string.Empty,
+                        checkPoint04       = frame.TimeAtCheckPoint("S4" )?.ToString() ?? string.Empty,
+                        checkPoint05       = frame.TimeAtCheckPoint("S5" )?.ToString() ?? string.Empty,
+                        checkPoint06       = frame.TimeAtCheckPoint("S6" )?.ToString() ?? string.Empty,
+                        checkPoint07       = frame.TimeAtCheckPoint("S7" )?.ToString() ?? string.Empty,
+                        checkPoint08       = frame.TimeAtCheckPoint("S8" )?.ToString() ?? string.Empty,
+                        checkPoint09       = frame.TimeAtCheckPoint("S9" )?.ToString() ?? string.Empty,
+                        checkPoint10       = frame.TimeAtCheckPoint("S10")?.ToString() ?? string.Empty,
+                        checkPoint11       = frame.TimeAtCheckPoint("S11")?.ToString() ?? string.Empty,
+                        checkPoint12       = frame.TimeAtCheckPoint("S12")?.ToString() ?? string.Empty,
 
-                time_01_02         : frame.TimeFromCheckPointToCheckPoint( "S1" , "S2" )?.ToString() ?? string.Empty,
-                time_02_03         : frame.TimeFromCheckPointToCheckPoint( "S2" , "S3" )?.ToString() ?? string.Empty,
-                time_03_04         : frame.TimeFromCheckPointToCheckPoint( "S3" , "S4" )?.ToString() ?? string.Empty,
-                time_04_05         : frame.TimeFromCheckPointToCheckPoint( "S4" , "S5" )?.ToString() ?? string.Empty,
-                time_05_06         : frame.TimeFromCheckPointToCheckPoint( "S5" , "S6" )?.ToString() ?? string.Empty,
-                time_06_07         : frame.TimeFromCheckPointToCheckPoint( "S6" , "S7" )?.ToString() ?? string.Empty,
-                time_07_08         : frame.TimeFromCheckPointToCheckPoint( "S7" , "S8" )?.ToString() ?? string.Empty,
-                time_08_09         : frame.TimeFromCheckPointToCheckPoint( "S8" , "S9" )?.ToString() ?? string.Empty,
-                time_09_10         : frame.TimeFromCheckPointToCheckPoint( "S9" , "S10")?.ToString() ?? string.Empty,
-                time_10_11         : frame.TimeFromCheckPointToCheckPoint( "S10", "S11")?.ToString() ?? string.Empty,
-                time_11_12         : frame.TimeFromCheckPointToCheckPoint( "S11", "S12")?.ToString() ?? string.Empty
-            )
+                        time_01_02         = frame.TimeFromCheckPointToCheckPoint( "S1" , "S2" )?.ToString() ?? string.Empty,
+                        time_02_03         = frame.TimeFromCheckPointToCheckPoint( "S2" , "S3" )?.ToString() ?? string.Empty,
+                        time_03_04         = frame.TimeFromCheckPointToCheckPoint( "S3" , "S4" )?.ToString() ?? string.Empty,
+                        time_04_05         = frame.TimeFromCheckPointToCheckPoint( "S4" , "S5" )?.ToString() ?? string.Empty,
+                        time_05_06         = frame.TimeFromCheckPointToCheckPoint( "S5" , "S6" )?.ToString() ?? string.Empty,
+                        time_06_07         = frame.TimeFromCheckPointToCheckPoint( "S6" , "S7" )?.ToString() ?? string.Empty,
+                        time_07_08         = frame.TimeFromCheckPointToCheckPoint( "S7" , "S8" )?.ToString() ?? string.Empty,
+                        time_08_09         = frame.TimeFromCheckPointToCheckPoint( "S8" , "S9" )?.ToString() ?? string.Empty,
+                        time_09_10         = frame.TimeFromCheckPointToCheckPoint( "S9" , "S10")?.ToString() ?? string.Empty,
+                        time_10_11         = frame.TimeFromCheckPointToCheckPoint( "S10", "S11")?.ToString() ?? string.Empty,
+                        time_11_12         = frame.TimeFromCheckPointToCheckPoint( "S11", "S12")?.ToString() ?? string.Empty
+                    }
         )
     );
 }
 #endregion
 
 #region dump
-await using var writer = new StreamWriter(pathOut, append: false, Encoding.UTF8);
-await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
-await csv.WriteRecordsAsync(records.OrderBy(p => int.Parse(p.bib)));
-#endregion
+await using var writer = new StreamWriter(pathOut, append: false, System.Text.Encoding.UTF8);
+await using var csv = new CsvHelper.CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture);
+await csv.WriteRecordsAsync(records);
+
 Console.WriteLine("--------------------");
 Console.WriteLine("save data");
 Console.WriteLine("--------------------");
+#endregion
+
 Console.WriteLine("done");
 
